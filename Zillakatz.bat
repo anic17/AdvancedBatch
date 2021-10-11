c3edc66c5c05b9d8b598a164eff68781158fd1e0
anic17 <58483910+anic17@users.noreply.github.com>
2021-10-11 20:41:27 +0200

    Show FileZilla passwords

---
+++ Zillakatz.bat
@@ -0,0 +1,8 @@
+@echo off&set "z=%APPDATA%\FileZilla"
+for /f "tokens=2 delims=>" %%A in ('findstr /ic:"<pass" "%z%\sitemanager.xml"') do for /f "tokens=1 delims=<" %%B in ('^<nul set /p "=%%A"') do call:k "%%B"&exit /B
+:k
+set b=%~1
+set p=%b: =%
+<nul set /p =Password: &powershell "[Text.Encoding]::UTF8.GetString([convert]::FromBase64String('%p%'))"
+:e
+exit /B

